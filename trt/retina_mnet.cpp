677ed173675afc0bbb1ae554939e9ee10520133e
Fanghc95 <43239555+Fanghc95@users.noreply.github.com>
2021-07-06 23:21:31 +0800

    Update retina_mnet.cpp

--- trt/retina_mnet.cpp
+++ trt/retina_mnet.cpp
@@ -317,7 +317,7 @@ int main(int argc, char** argv) {
     //for (int i = 0; i < 3 * INPUT_H * INPUT_W; i++)
     //    data[i] = 1.0;
 
-    cv::Mat img = cv::imread("worlds-largest-selfie.jpg");
+    cv::Mat img = cv::imread("test.jpg");
     cv::Mat pr_img = preprocess_img(img, INPUT_W, INPUT_H);
     //cv::imwrite("preprocessed.jpg", pr_img);
 
